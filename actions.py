246facffc6ba279a56b244215e7c057dbc061172
Ankursrivastavaaa <ankursrivastavaaa@gmail.com>
2020-04-25 19:38:13 +0530

    Form Validation for location

--- actions.py
+++ actions.py
@@ -1,174 +1,228 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
+from typing import Dict, Text, Any, List, Union, Optional
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from numpy import loadtxt
+import zomatopy
+import json
+import smtplib
+from email.message import EmailMessage
+from concurrent.futures import ThreadPoolExecutor
 from rasa_sdk.events import AllSlotsReset
 from rasa_sdk.events import Restarted
 
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
-import zomatopy
-import json
-from numpy import loadtxt
-import smtplib
-from email.message import EmailMessage
-from concurrent.futures import ThreadPoolExecutor
 
 d_email_rest = []
 
-class ActionSearchRestaurants(Action):
-    def name(self):
-        return 'action_search_restaurants'
-    
-    def run(self, dispatcher, tracker, domain):
+class RestaurantForm(FormAction):
+    """custom form action"""
+
+    def name(self) -> Text:
+        return "restaurant_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        return ["location", "cuisine", "budget"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        return {
+            "cuisine": self.from_entity(entity="cuisine", intent="request_restaurant"),
+            "location": self.from_entity(entity="location", intent="request_restaurant"),
+            "budget": self.from_entity(entity="budget", intent="request_restaurant"),
+        }
+
+    # USED FOR DOCS: do not rename without updating in docs
+    @staticmethod
+    def cuisine_db() -> List[Text]:
+        return [
+            "chinese",
+            "italian",
+            "south indian",
+            "north indian",
+            "american",
+            "mexican",
+        ]
+    @staticmethod
+    def location_db() -> List[Text]:
+        location_list = loadtxt("data/locations.txt",dtype=str, delimiter="\n", unpack=False)
+        return location_list
+
+    def validate_location(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        print("Location:",value)
+        if isinstance(value, list):
+            value = value[0] 
+        if value.lower() in self.location_db():
+            return {"location": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_location")
+            return {"location": None}
+    # USED FOR DOCS: do not rename without updating in docs
+    def validate_cuisine(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+        print("Cusisine:",value)
+        if isinstance(value, list):
+            value = value[0] 
+        if value.lower() in self.cuisine_db():
+            return {"cuisine": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_cuisine")
+            return {"cuisine": None}
+
+    def validate_budget(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        print("Budget:",value)
+        if isinstance(value, list):
+            value = value[0] 
+        if value.lower() in ["low","mid","high"]:
+            return {"budget": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_budget")
+            return {"budget": None}
+
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
         config={ "user_key":"47ef160bda39996b2dbaff7f9fd0e554"}
         zomato = zomatopy.initialize_app(config)
+        actionListener = tracker.get_slot("actionListener")
         loc = tracker.get_slot('location')
         cuisine = tracker.get_slot('cuisine')
-        results, lat, lon = self.get_location_suggestions(loc,zomato)
+        budget = tracker.get_slot('budget')
+        print(cuisine,budget,loc)
+        return []
+        # results, lat, lon = self.get_location_suggestions(loc,zomato)
 
-        if (results == 0):
-            # Zomato API could not find suggestions for this location.
-            restaurant_exist = False
-            dispatcher.utter_message("Sorry, no results found in this location:("+ "\n")
-        else:
-            d_rest = self.get_restaurants(lat, lon, cuisine)
+    #     if (results == 0):
+    #         # Zomato API could not find suggestions for this location.
+    #         restaurant_exist = False
+    #         dispatcher.utter_message("Sorry, no results found in this location:("+ "\n")
+    #     else:
+    #         d_rest = self.get_restaurants(lat, lon, cuisine)
             
-            d_budget_rating_sorted = sorted(
-                    d_rest, key=lambda k: k['restaurant']['user_rating']['aggregate_rating'], reverse=True)
+    #         d_budget_rating_sorted = sorted(
+    #                 d_rest, key=lambda k: k['restaurant']['user_rating']['aggregate_rating'], reverse=True)
 
-            # Build the response
-            response = ""
-            restaurant_exist = False
-            if len(d_budget_rating_sorted) == 0:
-                dispatcher.utter_message("Sorry, no results found :("+ "\n")
-            else:
-                # Pick the top 5
-                d_budget_rating_top5 = d_budget_rating_sorted[:5]
-                global d_email_rest
-                d_email_rest = d_budget_rating_sorted[:10]
-                if(d_email_rest and len(d_email_rest) > 0):
-                    restaurant_exist = True
-                for restaurant in d_budget_rating_top5:
-                    response=response+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+"\n" + " has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n" + "\n"
+    #         # Build the response
+    #         response = ""
+    #         restaurant_exist = False
+    #         if len(d_budget_rating_sorted) == 0:
+    #             dispatcher.utter_message("Sorry, no results found :("+ "\n")
+    #         else:
+    #             # Pick the top 5
+    #             d_budget_rating_top5 = d_budget_rating_sorted[:5]
+    #             global d_email_rest
+    #             d_email_rest = d_budget_rating_sorted[:10]
+    #             if(d_email_rest and len(d_email_rest) > 0):
+    #                 restaurant_exist = True
+    #             for restaurant in d_budget_rating_top5:
+    #                 response=response+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+"\n" + " has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n" + "\n"
                 
-                dispatcher.utter_message("-----"+response)
-        return [SlotSet('location',loc)]
+    #             dispatcher.utter_message("-----"+response)
+    #     return [SlotSet('location',loc)]
+
+    # def get_location_suggestions(self, loc, zomato):
+    #     # Get location details including latitude and longitude
+    #     location_detail = zomato.get_location(loc, 1)
+    #     d1 = json.loads(location_detail)
+    #     lat = 0
+    #     lon = 0
+    #     results = len(d1["location_suggestions"])
+    #     if (results > 0):
+    #         lat = d1["location_suggestions"][0]["latitude"]
+    #         lon = d1["location_suggestions"][0]["longitude"]
+    #     return results, lat, lon
+
+    # def get_restaurants(self, lat, lon, cuisine):
+    #     cuisines_dict = {'american': 1, 'chinese': 25, 'italian': 55,
+    #                         'mexican': 73, 'north indian': 50, 'south indian': 85}
+    #     d_rest = []
+    #     executor = ThreadPoolExecutor(max_workers=5)
+    #     for res_key in range(0, 101, 20):
+    #         executor.submit(retrieve_restaurant, lat, lon, cuisines_dict, cuisine, res_key, d_rest)
+    #     executor.shutdown()
+    #     return d_rest
+
+    # def retrieve_restaurant(self ,lat, lon, cuisines_dict, cuisine, res_key, d_rest):
+    #     base_url = "https://developers.zomato.com/api/v2.1/"
+    #     headers = {'Accept': 'application/json',
+    #                 'user-key': '5787bb8301dd97fbe86ec40febf7e03b'}
+    #     try:
+    #         results = (requests.get(base_url + "search?" + "&lat=" + str(lat) + "&lon=" + str(lon) + "&cuisines=" + str(
+    #             cuisines_dict.get(cuisine)) + "&start=" + str(res_key)+"&count=20", headers=headers).content).decode("utf-8")
+    #     except:
+    #         return
+    #     d = json.loads(results)
+    #     d_rest.extend(d['restaurants'])
     
-    def get_location_suggestions(self, loc, zomato):
-        # Get location details including latitude and longitude
-        location_detail = zomato.get_location(loc, 1)
-        d1 = json.loads(location_detail)
-        lat = 0
-        lon = 0
-        results = len(d1["location_suggestions"])
-        if (results > 0):
-            lat = d1["location_suggestions"][0]["latitude"]
-            lon = d1["location_suggestions"][0]["longitude"]
-        return results, lat, lon
-
-    def get_restaurants(self, lat, lon, cuisine):
-        cuisines_dict = {'american': 1, 'chinese': 25, 'italian': 55,
-                         'mexican': 73, 'north indian': 50, 'south indian': 85}
-        d_rest = []
-        executor = ThreadPoolExecutor(max_workers=5)
-        for res_key in range(0, 101, 20):
-            executor.submit(retrieve_restaurant, lat, lon, cuisines_dict, cuisine, res_key, d_rest)
-        executor.shutdown()
-        return d_rest
-
-
+    
+   
 
 # Send email the list of 10 restaurants
 class ActionSendEmail(Action):
     def name(self):
         return 'action_send_email'
-    
+
     def run(self, dispatcher, tracker, domain):
         # Get user's email id
-        to_email = tracker.get_slot('emailid')
+        to_email = tracker.get_slot('email')
 
         # Get location and cuisines to put in the email
         loc = tracker.get_slot('location')
         cuisine = tracker.get_slot('cuisine')
-        global d_email_rest
-        email_rest_count = len(d_email_rest)
-        # Construct the email 'subject' and the contents.
-        d_email_subj = "Top " + str(email_rest_count) + " " + cuisine.capitalize() + " restaurants in " + str(loc).capitalize()
-        d_email_msg = "Hi there! Here are the " + d_email_subj + "." + "\n" + "\n" +"\n"
-        for restaurant in d_email_rest:
-            d_email_msg = d_email_msg + restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+" has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n" +"\n"
+        budget = tracker.get_slot('budget')
+        print(cuisine,budget,loc,to_email)
+        return [AllSlotsReset()]
+        # global d_email_rest
+        # email_rest_count = len(d_email_rest)
+        # # Construct the email 'subject' and the contents.
+        # d_email_subj = "Top " + str(email_rest_count) + " " + cuisine.capitalize() + " restaurants in " + str(loc).capitalize()
+        # d_email_msg = "Hi there! Here are the " + d_email_subj + "." + "\n" + "\n" +"\n"
+        # for restaurant in d_email_rest:
+        #     d_email_msg = d_email_msg + restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+" has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n" +"\n"
 
         
-        # Open SMTP connection to our email id.
-        s = smtplib.SMTP("smtp.gmail.com", 587)
-        s.starttls()
-        s.login("neerajankurchatbotproject@gmail.com", "iiitbchatbot")
+        # # Open SMTP connection to our email id.
+        # s = smtplib.SMTP("smtp.gmail.com", 587)
+        # s.starttls()
+        # s.login("neerajankurchatbotproject@gmail.com", "iiitbchatbot")
 
-        # Create the msg object
-        msg = EmailMessage()
+        # # Create the msg object
+        # msg = EmailMessage()
 
-        # Fill in the message properties
-        msg['Subject'] = d_email_subj
-        msg['From'] = "neerajankurchatbotproject@gmail.com"
+        # # Fill in the message properties
+        # msg['Subject'] = d_email_subj
+        # msg['From'] = "neerajankurchatbotproject@gmail.com"
 
-        # Fill in the message content
-        msg.set_content(d_email_msg)
-        msg['To'] = to_email
+        # # Fill in the message content
+        # msg.set_content(d_email_msg)
+        # msg['To'] = to_email
 
-        s.send_message(msg)
-        s.quit()
-        dispatcher.utter_message("**** EMAIL SENT! HAPPY DINING :) ****")
-        return []
+        # s.send_message(msg)
+        # s.quit()
+        # dispatcher.utter_message("**** EMAIL SENT! HAPPY DINING :) ****")
 
-# Check if the location exists, else utter not found.
-class ActionValidateLocation(Action):
-    def name(self):
-        return 'action_validate_location'
-    
-    def run(self, dispatcher, tracker, domain):
-        location_list = loadtxt("data/locations.txt",dtype=str, delimiter="\n", unpack=False)
-        loc = tracker.get_slot('location')
-
-        city = str(loc)
-        print(city)
-        if(city.lower() not in location_list):
-            dispatcher.utter_message(template="utter_wrong_location")
-        
-# Check if the location exists, else utter not found.
-class ActionValidateBudget(Action):
-	def name(self):
-		return 'action_validate_budget'
-
-	def run(self, dispatcher, tracker, domain):
-		bud = tracker.get_slot('budget')
-		budget = str(bud)
-		print(budget)
-		budget_list = ["low","medium","high"]
-		if(budget.lower() not in budget_list):
-			dispatcher.utter_message(template="utter_wrong_budget")
-
-# Check if the location exists, else utter not found.
-class ActionValidateCuisine(Action):
-	def name(self):
-		return 'action_validate_cuisine'
-
-	def run(self, dispatcher, tracker, domain):
-		cus = tracker.get_slot('cuisine')
-		cuisine = str(cus)
-		print(cuisine)
-		cuisine_list = ["chinese","italian","south indian","north indian","american","mexican"]
-		if(cuisine.lower() not in cuisine_list):
-			dispatcher.utter_message(template="utter_wrong_cuisine")
-
-
-def retrieve_restaurant(lat, lon, cuisines_dict, cuisine, res_key, d_rest):
-    base_url = "https://developers.zomato.com/api/v2.1/"
-    headers = {'Accept': 'application/json',
-                'user-key': '5787bb8301dd97fbe86ec40febf7e03b'}
-    try:
-        results = (requests.get(base_url + "search?" + "&lat=" + str(lat) + "&lon=" + str(lon) + "&cuisines=" + str(
-            cuisines_dict.get(cuisine)) + "&start=" + str(res_key)+"&count=20", headers=headers).content).decode("utf-8")
-    except:
-        return
-    d = json.loads(results)
-    d_rest.extend(d['restaurants'])
+   
--- data/nlu.md
+++ data/nlu.md
@@ -1,73 +1,51 @@
 ## intent:greet
-- hey.
-- hello.
-- hi.
-- hey there.
-- Heyyy
-- hey !!!!!
-- Holaa
 - Hi
-- Hola
 - Hey
-- Hello !!
-- Hello!
+- Hi bot
+- Hey bot
+- Hello
+- Good morning
+- hi again
+- hi folks
+- hi Mister
+- hi pal!
+- hi there
+- greetings
+- hello everybody
+- hello is anybody there
+- hello robot
+- hallo
+- heeey
+- hi hi
+- hey
+- hey hey
+- hello there
+- hi
+- hello
+- yo
+- hola
+- hi?
+- hey bot!
+- hello friend
+- Holaaaa
 
-## intent:goodbye
-- bye
-- goodbye
-- see you around
-- see you later
-
-## intent:affirm
-- yesrasa
-- indeed
-- of course
-- that sounds good
-- correct
-- yes
-- yes. Please
-- Yes
-
-## intent:deny
-- no
-- never
-- I don't think so
-- don't like that
-- no way
-- not really
-- No
-- no thanks
-- no. thanks
-
-## intent:mood_great
-- perfect
-- very good
-- great
-- amazing
-- wonderful
-- I am feeling very good
-- I am great
-- I'm good
-
-## intent:mood_unhappy
-- sad
-- very sad
-- unhappy
-- bad
-- very bad
-- awful
-- terrible
-- not very good
-- extremely sad
-- so sad
-
-## intent:bot_challenge
-- are you a bot?
-- are you a human?
-- am I talking to a bot?
-- am I talking to a human?
-
-## intent:restaurant_search
+## intent:request_restaurant
+- im looking for a restaurant
+- can i get [Chinese](cuisine) food in any area
+- a restaurant that serves [North Indian](cuisine) food
+- id like a restaurant
+- im looking for a restaurant that serves [South Indian](cuisine) food
+- can i find a restaurant that serves [American](cuisine)
+- i am looking for any place that serves [Italian](cuisine) food for three
+- i need to find a restaurant
+- uh im looking for a restaurant that serves [Mexican](cuisine) food
+- uh can i find a restaurant and it should serve [North Indian](cuisine) food
+- im looking for a restaurant serving [italian](cuisine) food
+- restaurant please
+- i'd like to book a table for two with [Mexican](cuisine) cuisine
+- book me a table for three at the [italian](cuisine) restaurant
+- looking for a table at the [mexican](cuisine) restaurant for five
+- Can I get a table for four at the place which server [Southy](cuisine:South Indian) food?
 - i'm looking for a place to eat
 - I want to grab lunch
 - I am searching for a dinner spot
@@ -81,13 +59,20 @@
 - Okay. Show me some in [Pune](location)
 - I am looking for [Mexican](cuisine)
 - [300 to 700](budget:mid)
-- Can you suggest some good restaurants in [kolkata](location)
+- Can you suggest some good restaurants in [delli](location:delhi)
 - [american](cuisine)
 - [<300](budget:low)
+- [< than 300](budget:low)
+- [cheap food](budget:low)
+- [below 300](budget:low)
 - in mubaim
 - [Mumbai](location)
-- Im hungry. Looking out for some good [chinese](cuisine) restaurants in [chandigarh](location)
+- Im hungry. Looking out for some good [chines](cuisine:chinese) restaurants in [chandigarh](location)
 - [more than 700](budget:high)
+- [expensive](budget:high)
+- [> 700](budget:high)
+- [greater than 700](budget:high)
+- [more than Rs 700](budget:high)
 - Get me a resturant
 - [mankapur](location)
 - [agra](location)
@@ -95,6 +80,11 @@
 - [costly](budget:high) one
 - Ill prefer [North Indian](cuisine)
 - [300-700](budget:mid) range
+- [between 300 to 700](budget:mid) range
+- [medium range](budget:mid) range
+- [mid range](budget:mid) range
+- [Not very costly](budget:mid) range
+- [300 to 700](budget:mid) range
 - looking for [South Indian](cuisine)
 - Can you suggest some good restaurants in [Buxar](location)
 - find some in [Amaravati](location)
@@ -104,6 +94,11 @@
 - [Less than Rs 300](budget:low)
 - find me a resturant
 - [Bangalore](location)
+- Looking for a resturant
+- [North Indian](cuisine) will do
+- [Delhi](location)
+- [300-700](budget:mid)
+- [300-700 range](budget:mid)
 
 ## intent:send_email
 - yes send it to [abc@gmail.com](email)
@@ -111,12 +106,139 @@
 - Yes send it to [ankursrivastavaaa@gmail.com](email)
 - [android@domain.com](email)
 - [ank@fro.com](email)
+- [ankur@gmail.com](email)
 
-## synonym:4
-- four
+## intent:affirm
+- yeah a cheap restaurant serving international food
+- correct
+- ye
+- uh yes
+- let's do it
+- yeah
+- um yes
+- yes knocking
+- that's correct
+- yes yes
+- right
+- yea
+- yes
+- yes right
+- yes and i dont care
+- right on
+- i love that
+- Yes
+
+## intent:deny
+- no
+- no new selection
+- no thanks
+- no thank you
+- uh no
+- breath no
+- do you have something else
+- no this does not work for me
+
+## intent:thankyou
+- um thank you good bye
+- okay cool uh good bye thank you
+- okay thank you good bye
+- you rock
+- and thats all thank you and good bye
+- thank you and good bye
+- sorry about my mistakes thank you good bye
+- noise thank you good bye
+- thank you goodbye noise
+- okay thank you goodbye
+- uh thank you good bye
+- thank you goodbye
+- thank you goodbye noise thank you goodbye
+- breath thank you goodbye
+- thank you
+- okay thank you
+- thanks goodbye
+- ah thank you goodbye
+- thank you noise
+- thank you good bye
+- breath thank you very much goodbye
+- thanks
+- noise thank you goodbye
+- unintelligible thank you goodbye
+- uh okay thank you good bye
+- thank you bye
+- um okay thank you good bye
+
+## intent:chitchat
+- can you share your boss with me?
+- i want to get to know your owner
+- i want to know the company which designed you
+- i want to know the company which generated you
+- i want to know the company which invented you
+- i want to know who invented you
+- May I ask who invented you?
+- please tell me the company who created you
+- please tell me who created you
+- tell me more about your creators
+- tell me more about your founders
+- Ahoy matey how are you?
+- are you alright
+- are you having a good day
+- Are you ok?
+- are you okay
+- Do you feel good?
+- how are things going
+- how are things with you?
+- How are things?
+- how are you
+- how are you doing
+- how are you doing this morning
+- how are you feeling
+- how are you today
+- How are you?
+- How is the weather today?
+- What's the weather like?
+- How is the weather?
+- What is the weather at your place?
+- Do you have good weather?
+- Is it raining?
+- What's it like out there?
+- Is it hot or cold?
+- Beautiful day, isn't it?
+- What's the weather forecast?
+- Is it quite breezy outside?
+
+## intent:stop
+- ok then you cant help me
+- that was shit, you're not helping
+- you can't help me
+- you can't help me with what i need
+- i guess you can't help me then
+- ok i guess you can't help me
+- that's not what i want
+- ok, but that doesnt help me
+- this is leading to nothing
+- this conversation is not really helpful
+- you cannot help me with what I want
+- I think you cant help me
+- hm i don't think you can do what i want
+- stop
+- stop go back
+- do you get anything?
+- and you call yourself bot company? pff
+- and that's it?
+- nothing else?
+
+## intent:bot_challenge
+- are you a bot?
+- are you a human?
+- am I talking to a bot?
+- am I talking to a human?
 
 ## synonym:Delhi
 - New Delhi
+- Delli
+
+## synonym:South Indian
+- Southy
 
 ## synonym:bangalore
 - bengaluru
@@ -129,29 +251,42 @@
 
 ## synonym:high
 - more than 700
-- costly
 - expensive
+- > 700
+- >700
+- greater than 700
+- more than Rs 700
+- costly
 - top
 - best one
 
 ## synonym:low
 - less than 300
 - <300
+- < than 300
+- cheap food
+- below 300
 - cheapest
 - Less than Rs 300
 - cheap
 - low bugdet
+- low
 
 ## synonym:mid
 - 300 to 700
 - 300-700
+- between 300 to 700
+- medium range
+- mid range
+- Not very costly
 - midium
 - between 300-700
-- between 300 to 700
 - between 300 - 700
 - moderate priced
 - not very high
 - middle class
+- mid
+- medium
 
 ## regex:email
 - ^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$
